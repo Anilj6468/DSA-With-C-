146001d682af225718566a4918d7f49726e13433
Anilj6468 <Anilj3517@gmail.com>
2026-04-08 21:51:45 +0530

    Done

--- function.cpp
+++ function.cpp
@@ -1,111 +1,46 @@
-#include <iostream>;
+#include <iostream>
 using namespace std;
 
-
-int sum(int a, int b , int c)
-{
-    return a + b + c;
-    return a + b + c;
-}
-
-int main()
-{
-    int result = sum(1, 2, 5);
-    cout << "Sum is: " << result << endl;
-    cout << "Sum is: " << result << endl;
-    cout << "Sum is: " << result << endl;
-    return 0;
-}
-
-
-float divide(float a, float b, float c)
-{
-    return a / b / c;
-}
-
-int main()
-{
-    float result = divide(1, 2, 5);
-    cout << "Divide is: " << result << endl;
-    return 0;
-}
-
-float add(float a, float b) {
-    return a + b;
-}
-
-float subtract(float a, float b) {
-    return a - b;
-}
-
-float multiply(float a, float b) {
-    return a * b;
-}
-
-float divide(float a, float b) {
-    if (b == 0) {
-        printf("Error! Division by zero is not allowed.\n");
-        return 0;
+int main(){
+    int a=20, b=5;
+    char ope;
+    cout << "Enter Operator:";
+    cin>>ope; //+
+    
+    switch(ope){
+        case '+';
+        cout << "Sum:" << a+b;
+        break;
+        case '-';
+        cout << "Sub:" << a-b;
+        break;
+        case '*';
+        cout << "Mul:" << a*b;
+        break;
+        case '/';
+        cout << "Div:" << a/b;
+        break;
+        default:
+        cout << "Invalid";
     }
-    return a / b;
-}
-
-int modulus(int a, int b) {
-    if (b == 0) {
-        printf("Error! Modulus by zero is not allowed.\n");
+    
+    // int a=2;
+    // switch(a){
+    //     case 1:
+    //     cout << "One:";
+    //     break;
+    //     case 2:
+    //     cout << "Two:";
+    //     break;
+    //     case 3:
+    //     cout << "Three:";
+    //     break;
+    //     case 4:
+    //     cout << "Four:";
+    //     break;
+    //     default:
+    //     cout << "Invalid";
+        
+    // }
         return 0;
-    }
-    return a % b;
-}
-
-int main() {
-    int choice;
-    float num1, num2;
-    char cont;
-
-    do {
-        printf("\n===== Arithmetic Menu =====\n");
-        printf("1. Addition (+)\n");
-        printf("2. Subtraction (-)\n");
-        printf("3. Multiplication (*)\n");
-        printf("4. Division (/)\n");
-        printf("5. Modulus (%%)\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        printf("Enter two numbers: ");
-        scanf("%f %f", &num1, &num2);
-
-        switch (choice) {
-            case 1:
-                printf("Result = %.2f\n", add(num1, num2));
-                break;
-
-            case 2:
-                printf("Result = %.2f\n", subtract(num1, num2));
-                break;
-
-            case 3:
-                printf("Result = %.2f\n", multiply(num1, num2));
-                break;
-
-            case 4:
-                printf("Result = %.2f\n", divide(num1, num2));
-                break;
-
-            case 5:
-                printf("Result = %d\n", modulus((int)num1, (int)num2));
-                break;
-
-            default:
-                printf("Invalid choice!\n");
-        }
-
-        printf("\nPress 'e' to exit or any other key to continue: ");
-        scanf(" %c", &cont);
-
-    } while (cont != 'e' && cont != 'E');
-
-    printf("Program terminated.\n");
-    return 0;
 }
