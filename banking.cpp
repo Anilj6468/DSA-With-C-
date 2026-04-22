cc84c3d22bcdd7d9be4e435325ad21b7829ee3cc
Anilj6468 <Anilj3517@gmail.com>
2026-04-22 21:47:48 +0530

    In Progress

--- banking.cpp
+++ banking.cpp
@@ -1,53 +1,125 @@
 #include <iostream>
 using namespace std;
-
-// Base Class
 class BankAccount {
 protected:
-    int accountNumber;
     string accountHolderName;
+    string accountNumber;
     double balance;
 
 public:
-    BankAccount(int accNo, string name, double bal) {
-        accountNumber = accNo;
+    BankAccount(string name, string accNo, double bal) {
         accountHolderName = name;
+        accountNumber = accNo;
         balance = bal;
     }
 
-    void displayDetails() {
+    void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            cout << "Deposited: " << amount << endl;
+        } else {
+            cout << "Invalid deposit amount!" << endl;
+        }
+    }
+
+    virtual void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            cout << "Withdrawn: " << amount << endl;
+        } else {
+            cout << "Insufficient balance!" << endl;
+        }
+    }
+
+    double getBalance() {
+        return balance;
+    }
+
+    void displayAccountInfo() {
+        cout << "\nAccount Holder: " << accountHolderName << endl;
         cout << "Account Number: " << accountNumber << endl;
-        cout << "Account Holder Name: " << accountHolderName << endl;
         cout << "Balance: " << balance << endl;
     }
 };
 
-// Derived Class
 class SavingAccount : public BankAccount {
 private:
     double interestRate;
 
 public:
-    // Constructor
-    SavingAccount(int accNo, string name, double bal, double rate)
-        : BankAccount(accNo, name, bal) {
+    SavingAccount(string name, string accNo, double bal, double rate)
+        : BankAccount(name, accNo, bal) {
         interestRate = rate;
     }
 
-    // Method to calculate interest
     void calculateInterest() {
-        double interest = (balance * interestRate) / 100;
-        cout << "Interest: " << interest << endl;
+        double interest = balance * interestRate / 100;
+        cout << "Saving Account Interest: " << interest << endl;
+    }
+};
+
+class CheckingAccount : public BankAccount {
+private:
+    double overdraftLimit;
+
+public:
+    CheckingAccount(string name, string accNo, double bal, double limit)
+        : BankAccount(name, accNo, bal) {
+        overdraftLimit = limit;
+    }
+
+    void withdraw(double amount) override {
+        if (amount <= balance + overdraftLimit) {
+            balance -= amount;
+            cout << "Withdrawn (with overdraft): " << amount << endl;
+        } else {
+            cout << "Overdraft limit exceeded!" << endl;
+        }
+    }
+
+    void checkOverDraft() {
+        if (balance < 0) {
+            cout << "Account is in overdraft!" << endl;
+        } else {
+            cout << "No overdraft used." << endl;
+        }
     }
 };
 
 
+class FixedDepositAccount : public BankAccount {
+private:
+    int term;
+    double interestRate;
+
+public:
+    FixedDepositAccount(string name, string accNo, double bal, int t, double rate)
+        : BankAccount(name, accNo, bal) {
+        term = t;
+        interestRate = rate;
+    }
+
+    void calculateInterest() {
+        double interest = (balance * interestRate * term) / (12 * 100);
+        cout << "Fixed Deposit Interest: " << interest << endl;
+    }
+};
 
 int main() {
-    SavingAccount acc1(101, "John Doe", 5000, 5);
+    SavingAccount sa("Rahul", "SA123", 10000, 5);
+    sa.displayAccountInfo();
+    sa.deposit(2000);
+    sa.calculateInterest();
 
-    acc1.displayDetails();
-    acc1.calculateInterest();
+    
+    CheckingAccount ca("Amit", "CA456", 5000, 2000);
+    ca.displayAccountInfo();
+    ca.withdraw(6000); 
+    ca.checkOverDraft();
+
+    FixedDepositAccount fda("Neha", "FD789", 20000, 12, 6);
+    fda.displayAccountInfo();
+    fda.calculateInterest();
 
     return 0;
 }
